c5c8b57571c7160ccaaee4fb47adae2bd0b32f0c
Jesper Sørensen <jeppester1@gmail.com>
2023-11-29 15:01:35 +0100

    Prettier

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,10 +1,17 @@
-import { Title, Subtitle, Description, Primary, Controls, Stories, DocsContext } from '@storybook/blocks';
+import { useContext } from "react"
+import {
+  Title,
+  Subtitle,
+  Description,
+  Primary,
+  Controls,
+  Stories,
+  DocsContext,
+} from "@storybook/blocks"
 
 import prepareColorVariables from "../utils/prepareColorVariables"
 import colors from "../colors.json"
 import MuffibookTheme from "./MuffibookTheme"
-import { useContext } from 'react';
-
 
 const cssRoot = document.querySelector(":root")
 const cssVariables = prepareColorVariables(colors).cssVariables
@@ -14,9 +21,11 @@ Object.entries(cssVariables).forEach(([name, value]) =>
 )
 
 const SourceLink = () => {
-  const { attachedCSFFile: { meta: { component } } } = useContext(DocsContext)
-
-  console.log(useContext(DocsContext))
+  const {
+    attachedCSFFile: {
+      meta: { component },
+    },
+  } = useContext(DocsContext)
 
   if (!component) {
     return null
@@ -28,9 +37,30 @@ const SourceLink = () => {
     <>
       <h3>Sources</h3>
       <ul>
-        <li><a href={`https://github.com/abtion/component-library/blob/main/components/${name}/index.tsx`} target="_BLANK">TS</a></li>
-        <li><a href={`https://github.com/abtion/component-library/blob/main/components/${name}/index.scss`} target="_BLANK">CSS</a></li>
-        <li><a href={`https://github.com/abtion/component-library/blob/main/components/${name}/index.test.tsx`} target="_BLANK">Tests</a></li>
+        <li>
+          <a
+            href={`https://github.com/abtion/component-library/blob/main/components/${name}/index.tsx`}
+            target="_BLANK"
+          >
+            TS
+          </a>
+        </li>
+        <li>
+          <a
+            href={`https://github.com/abtion/component-library/blob/main/components/${name}/index.scss`}
+            target="_BLANK"
+          >
+            CSS
+          </a>
+        </li>
+        <li>
+          <a
+            href={`https://github.com/abtion/component-library/blob/main/components/${name}/index.test.tsx`}
+            target="_BLANK"
+          >
+            Tests
+          </a>
+        </li>
       </ul>
     </>
   )
