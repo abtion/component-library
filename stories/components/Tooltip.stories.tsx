61db79a1dad2d9b1dbf9544d4781642c6e9ac16d
MartinAbtion <mp@abtion.com>
2024-02-15 13:54:19 +0100

    Add story

---
+++ stories/components/Tooltip.stories.tsx
@@ -0,0 +1,17 @@
+import { type Meta, type StoryObj } from "@storybook/react"
+import React from "react"
+import { Tooltip, TooltipProps } from "~/components/Tooltip"
+
+const meta = {
+  title: "Components/🚧 Tooltip",
+  component: Tooltip,
+} satisfies Meta<typeof Tooltip>
+export default meta
+
+export const Default: StoryObj<typeof meta> = {
+  args: {
+    children: <p>paragraph</p>,
+    hoverText: "hover text",
+    customStyle: "",
+  } as TooltipProps,
+}
