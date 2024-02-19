c8504ce39d16651f7d26f3012bc08ab53f41f3bb
MartinAbtion <104842415+MartinAbtion@users.noreply.github.com>
2024-02-19 08:44:51 +0100

    Feature: Add card component from Infare (#601)
    * Add Card component
    
    * Add story
    
    * Add tests for new component

---
+++ components/Card/index.test.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import Card from "."
+import { render } from "@testing-library/react"
+import EnvelopeIcon from "@heroicons/react/20/solid/EnvelopeIcon"
+
+describe(Card, () => {
+  it("renders name, description and children", () => {
+    const { getByText } = render(
+      <Card icon=<EnvelopeIcon /> name="cardName" description="cardDescription">
+        <p>child paragraph</p>
+      </Card>
+    )
+
+    expect(getByText("cardName")).toBeInTheDocument()
+    expect(getByText("cardDescription")).toBeInTheDocument()
+    expect(getByText("child paragraph")).toBeInTheDocument()
+  })
+
+  it("renders a learnMore link", () => {
+    const { getByRole } = render(
+      <Card
+        icon=<EnvelopeIcon />
+        name="cardName"
+        description="cardDescription"
+        learnMoreUrl="/support"
+      >
+        <p>child paragraph</p>
+      </Card>
+    )
+    expect(getByRole("link")).toHaveTextContent("Learn more >")
+  })
+})
---
+++ components/Card/index.tsx
@@ -0,0 +1,65 @@
+import React from "react"
+import classNames from "classnames"
+
+import "./index.scss"
+
+export interface CardProps {
+  icon: JSX.Element
+  name: string
+  description: string
+  isDark?: boolean
+  learnMoreUrl?: string
+  children: React.ReactNode
+}
+
+export default function Card({
+  icon,
+  name,
+  description,
+  isDark = false,
+  learnMoreUrl,
+  children,
+}: CardProps): JSX.Element {
+  const productCardClassName = classNames(
+    "ProductCard",
+    isDark && "ProductCard--dark"
+  )
+  const titleClassName = classNames(
+    "ProductCard__title",
+    isDark && "ProductCard__title--dark"
+  )
+  const descriptionClassName = classNames(
+    "ProductCard__description",
+    isDark && "ProductCard__description--dark"
+  )
+  const learnMoreClassName = classNames(
+    "ProductCard__learnMore",
+    isDark && "ProductCard__learnMore--dark"
+  )
+
+  return (
+    <div className={productCardClassName}>
+      <div className="flex flex-col h-full">
+        <div className="w-8 h-8">{icon}</div>
+
+        <h2 className={titleClassName}>{name}</h2>
+
+        <div className="flex flex-1 ">
+          <span className={descriptionClassName}>
+            {description}
+
+            {learnMoreUrl ? (
+              <>
+                {" "}
+                <a href={learnMoreUrl} className={learnMoreClassName}>
+                  Learn more &gt;
+                </a>
+              </>
+            ) : null}
+          </span>
+        </div>
+        <div className="items-center flex justify-between h-4">{children}</div>
+      </div>
+    </div>
+  )
+}
