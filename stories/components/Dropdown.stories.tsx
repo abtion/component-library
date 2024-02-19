7b0222a5063259baab145e747457eca4696d2dcf
MartinAbtion <104842415+MartinAbtion@users.noreply.github.com>
2024-02-19 08:45:00 +0100

    Feature: Add Dropdown component from Infare (#600)
    * Add dropdown component
    
    * Add story
    
    * Ensure usehooks-ts is installed
    
    We are using that for the "click-outside" functionality
    
    * Add tests for the new dropdown
    
    * Fix lint-style issues

---
+++ stories/components/Dropdown.stories.tsx
@@ -0,0 +1,24 @@
+import { type Meta, type StoryObj } from "@storybook/react"
+import Dropdown, { DropdownProps, Sizes } from "~/components/Dropdown"
+
+const meta = {
+  title: "Components/🚧 Dropdown",
+  component: Dropdown,
+  argTypes: {
+    onClickAction: { action: true },
+  },
+} satisfies Meta<typeof Dropdown>
+export default meta
+
+export const Default: StoryObj<typeof meta> = {
+  args: {
+    active: { key: "1", text: "25" },
+    options: [
+      { key: "1", text: "25" },
+      { key: "2", text: "50" },
+      { key: "3", text: "100" },
+      { key: "4", text: "150" },
+    ],
+    size: Sizes.Medium,
+  } as DropdownProps,
+}
